369200dc9ffca36392b0a08f510894c013827ddb
josephrmoore <josephrmoore@gmail.com>
2014-03-10 02:12:28 -0400

    boxes interactable

---
+++ midterm/Assets/click.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class click : MonoBehaviour {
+
+	private bool over = false;
+	private bool active = false;
+	private Color c, white;
+
+	void OnMouseOver() 
+	{
+		over = true;
+	}
+	void OnMouseExit(){
+		over = false;
+	}
+	
+	// Use this for initialization
+	void Start () {
+		c = renderer.material.color;
+		white = new Color (1,1,1,1);
+		renderer.material.color = white;
+	}
+	
+	// Update is called once per frame
+	void Update () {		
+		if (Input.GetMouseButtonDown(0) && over) {
+			active = !active;
+		}
+		if(active){
+			renderer.material.color = c;
+		} else {
+			renderer.material.color = white;
+		}
+	}
+}
+
+
+
---
+++ midterm/Assets/rotate.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+	public float rotationSpeed = 10.0f;
+	public float lerpSpeed = 1.0f;
+	
+	private Vector3 speed = new Vector3();
+	private Vector3 avgSpeed = new Vector3();
+	private bool dragging = false;
+	private Vector3 targetSpeedX = new Vector3();
+	
+	void OnMouseOver() 
+	{
+		dragging = true;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton(0) && dragging) {
+			speed = new Vector3(-Input.GetAxis ("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+			avgSpeed = Vector3.Lerp(avgSpeed,speed,Time.deltaTime * 5);
+		} else {
+			if (dragging) {
+				speed = avgSpeed;
+				dragging = false;
+			}
+			float i = Time.deltaTime * lerpSpeed;
+			speed = Vector3.Lerp( speed, Vector3.zero, i);
+		}
+		
+		transform.Rotate(Vector3.back, speed.y * rotationSpeed, Space.World);
+	}
+}
+
+
+
